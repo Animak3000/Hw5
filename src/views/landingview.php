288f32e57d3cd0ba6534625f9f60bf32900f9683
michael's mac <enehnnamudi@gmail.com>
2016-12-11 19:45:24 -0800

    wip

--- src/views/landingview.php
+++ src/views/landingview.php
@@ -6,12 +6,14 @@ class landingview extends View{
 <!DOCTYPE html>
 <html lang='en'>
     <head>
+    	<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
         <title>Throw-a-Coin-in-the-Fountain</title>
     </head>
     <body>
         <h1>Throw-a-Coin-in-the-Fountain</h1>
     </body>
 </html>
+
 <?php
     }
 }
